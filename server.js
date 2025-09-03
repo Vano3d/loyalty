fe1ec13a8b18f3877474d1e85bcb2e9be98d7ed6
Ivan <vano3d@gmail.com>
2025-09-03 11:57:21 +0300

    save

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,14 @@ const DATA_FILE = 'data.json';
 
 app.use(cors());
 app.use(express.json());
+
+// Статика - публичные файлы фронтенда в папке public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Главная страница
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // GET: Получить данные
 app.get('/data', (req, res) => {
